d5bb25a80539b552a90fa73a520f58eda43ee7c0
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-05-12 16:20:59 +0200

    Add logo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Battlesnake Challange
 
+![Logo](logo.png)
+
 [![main](https://github.com/getcodelimit/pl-battlesnake/actions/workflows/main.yml/badge.svg)](https://github.com/getcodelimit/pl-battlesnake/actions/workflows/main.yml)
 [![Checked with Code Limit](https://codelimit.vercel.app/api/badge/getcodelimit/pl-battlesnake)](https://github.com/getcodelimit/codelimit)
 
@@ -13,3 +15,8 @@ For example, to launch a snake battle between JavaScript and Python run:
 ```shell
 ./start-game.sh javascript python
 ```
+
+## Languages
+
+- [C](c/)
+- [Python](python/)
--- c/snake.c
+++ c/snake.c
@@ -4,29 +4,30 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+int PORT = 3000;
+
 int main(int argc, char *argv[]) {
-    printf("Starting server...\n");
+    printf("Starting Battlesnake server on port: %d\n", PORT);
     int server_socket;
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(3000);
+    server_address.sin_port = htons(PORT);
     bind(server_socket, (struct sockaddr *)&server_address, 
         sizeof(server_address));
     listen(server_socket, 10);
     struct sockaddr client_address;
     socklen_t client_address_len = sizeof(client_address);
     while (1) {
-        int client_socket;
-        printf("Listening...\n");
-        client_socket = accept(server_socket, &client_address, 
+        int client_socket = accept(server_socket, &client_address, 
             &client_address_len);
-        printf("Connected!\n");
         int buffer_size = 32768;
         char *buffer = (char *)malloc(buffer_size * sizeof(char));
         size_t bytes = recv(client_socket, buffer, buffer_size, 0);
-        printf("Hello world!\n");
+        printf("%s\n", buffer);
+        
+        send(client_socket, response, response_len, 0);
         close(client_socket);
     }
 }
